0fd419522373b94d4c7f00be5a3fd842e33c1a3d
grisenti <emanuele@grisenti.net>
2023-01-19 10:12:40 +0100

    add CompilerError

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,37 @@
+use crate::Lexer;
+use std::fmt::Display;
 
+#[derive(Debug, PartialEq)]
+pub enum ErrorType {
+  Lexing,
+  Parsing,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct CompilerError {
+  line_no: u32,
+  line_str: String,
+  error_msg: String,
+  kind: ErrorType,
+}
+
+impl CompilerError {
+  pub fn from_lexer_state(lex: &Lexer, error_msg: String, kind: ErrorType) -> Self {
+    Self {
+      line_no: lex.line_no(),
+      line_str: lex.line().to_string(),
+      error_msg,
+      kind,
+    }
+  }
+}
+
+impl Display for CompilerError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "{:?} error at:\n  | \n{} | {}\n  | \n{}",
+      self.kind, self.line_no, self.line_str, self.error_msg
+    )
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod lexer;
+mod errors;
 
 use lexer::*;
+use errors::*;
 
 fn main() {
   let mut lex = Lexer::new("hello how are you");
