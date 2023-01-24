4c702037ea95f99a262a82c9ef9112c92ed98fdc
grisenti <emanuele@grisenti.net>
2023-01-24 19:40:59 +0100

    delete ast_printer and visitor trait

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,8 +1,6 @@
-mod ast_printer;
 mod expression;
 mod statement;
 
-pub use ast_printer::*;
 pub use expression::*;
 pub use statement::*;
 
@@ -10,15 +8,4 @@ pub enum ASTNode<'src> {
   Expr(Expr<'src>),
   Stmt(Stmt<'src>),
   Program(Vec<Stmt<'src>>),
-}
-
-pub trait NodeVisitor {
-  fn visit(&mut self, root: &ASTNode) {
-    match root {
-      ASTNode::Expr(exp) => self.visit_expr(&exp),
-      _ => {}
-    }
-  }
-
-  fn visit_expr(&mut self, expr: &Expr);
 }
