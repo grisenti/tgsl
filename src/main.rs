d7d212405b67cd56c5d9fedfcfc8d3a50503786c
grisenti <emanuele@grisenti.net>
2023-02-05 17:51:56 +0100

    tests: improve test results

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,15 @@ mod test {
     interpreter::*,
   };
 
+  macro_rules! test_file {
+    ($name:ident) => {
+      #[test]
+      fn $name() {
+        compile_and_run(&format!("tests/{}.wds", stringify!($name)));
+      }
+    };
+  }
+
   fn assert(_: &mut Interpreter, args: Vec<ExprValue>) -> InterpreterFnResult {
     if let ExprValue::Boolean(true) = args[0] {
       Ok(ExprValue::Null)
@@ -60,19 +69,19 @@ mod test {
     }
   }
 
-  #[test]
-  fn compile_and_run() {
-    for entry in fs::read_dir("tests/").unwrap() {
-      let path = entry.unwrap().path();
-      if path.is_file() {
-        let res = Compiler::compile(&fs::read_to_string(path).unwrap()).unwrap();
-        let mut interpreter = Interpreter::new(res);
-        interpreter.add_global_variable(
-          "assert",
-          ExprValue::Func(Rc::new(InterpreterFn::foreign(assert, 1))),
-        );
-        interpreter.interpret().unwrap();
-      }
-    }
+  fn compile_and_run(filename: &str) {
+    let res = Compiler::compile(&fs::read_to_string(filename).unwrap()).unwrap();
+    let mut interpreter = Interpreter::new(res);
+    interpreter.add_global_variable(
+      "assert",
+      ExprValue::Func(Rc::new(InterpreterFn::foreign(assert, 1))),
+    );
+    interpreter.interpret().unwrap();
   }
+
+  test_file!(closure_capture);
+  test_file!(function_declaration);
+  test_file!(passing_closures);
+  test_file!(primitive_operations);
+  test_file!(ufc_for_primitive_types);
 }
